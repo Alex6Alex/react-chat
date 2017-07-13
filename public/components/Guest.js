35355eebf5025bc18533691aa6927d78d6298a8d
Alex6Alex <alife4ever@mail.ru>
2017-07-13 17:11:16 +0300

    Add image to main page

---
+++ public/components/Guest.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Guest extends React.Component {
+
+  render(){
+    return(
+      <div className="guest_page">
+        <img className="welcome_image" src="../images/main.jpg"/>
+      </div>
+    )
+  }
+}
--- public/index.js
+++ public/index.js
@@ -4,10 +4,13 @@ import { Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
+import Guest from './components/Guest'
 import Chat from './components/Chat'
 
 export default class Index extends React.Component {
 	render(){
+		const signIn = false
+
 		return(
 			<html>
 				<head>
@@ -15,9 +18,9 @@ export default class Index extends React.Component {
 					<title>
 						Chat application
 					</title>
-					<link rel="stylesheet" 
-					href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-					integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+					<link rel="stylesheet"
+					href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+					integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
 					crossOrigin="anonymous"/>
 					<link rel="stylesheet" type="text/css" href="./styles/styles.css"/>
 				</head>
@@ -25,9 +28,9 @@ export default class Index extends React.Component {
 					<div className="wrapper">
 						<Header/>
 						<div id="page_container">
-							<SideBar/>
+							{signIn ? <SideBar/> : <Guest/>}
 							<Route path='/chat' component={Chat}/>
-							<Route path='/side' component={SideBar}/>				
+							<Route path='/side' component={SideBar}/>
 						</div>
 						<Footer/>
 					</div>
