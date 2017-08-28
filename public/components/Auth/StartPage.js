bf22d89932e961e5499a988ef66c55f135101a64
Alex6Alex <alife4ever@mail.ru>
2017-08-28 14:51:43 +0300

    add start page

--- public/components/Auth/StartPage.js
+++ public/components/Auth/StartPage.js
@@ -3,8 +3,25 @@ import React from 'react'
 export default class StartPage extends React.Component {
   render() {
     return(
-      <div>
-        HELLO!
+      <div className='start-page'>
+        <div className='greeting'>
+          <h1 className='greeting-text'>
+            Приветствуем Вас в нашем чате
+          </h1>
+          <h2 className='note'>
+            Для продолжения войдите или зарегистрируйтесь
+          </h2>
+        </div>
+        <div className='auth'>
+          <div className='auth-block'>
+            <div className='sign-in-block'>
+
+            </div>
+            <div className='sign-up-block'>
+
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
