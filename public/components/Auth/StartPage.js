82d5b263b62e73b8a059ed8163a8bb3fd5f12db5
Alex6Alex <alife4ever@mail.ru>
2017-09-10 10:55:58 +0300

    first version of start page

--- public/components/Auth/StartPage.js
+++ public/components/Auth/StartPage.js
@@ -1,23 +1,15 @@
 import React from 'react'
 
 import SignIn from './SignIn'
+import SignUp from './SignUp'
 
 export default class StartPage extends React.Component {
   render() {
     return(
       <div className='start-page'>
-        <div className='auth'>
-          <div className='auth-block'>
-            <SignIn/>
-            <div className='sign-up-block'>
-              <div className='sign-up-block-main'>
-                <h1>Регистрация</h1>
-                <p>Если Вы еще не зарегистрировались, то можете сделать это прямо сейчас.
-                  После быстрой регистрации Вы сразу сможете общаться со своими друзьями</p>
-                <button className='to-sign-up'>Регистрация</button>
-              </div>
-            </div>
-          </div>
+        <div className='auth-block'>
+          <SignIn/>
+          <SignUp/>
         </div>
       </div>
     )
